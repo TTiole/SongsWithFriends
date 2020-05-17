dc57c72c694fa935c47f37574c63f4f7cfeb0e02
iole <eloi@gonzalezurbano.com>
2020-05-17 10:07:07 -0700

    First commit. Set up the server and client skeleton

---
+++ server/index.js
@@ -0,0 +1,26 @@
+'use strict'
+const express = require('express')
+
+// Create the express app
+const app = express()
+
+// Routes and middleware
+// app.use(/* ... */)
+// app.get(/* ... */)
+
+// Error handlers
+app.use(function fourOhFourHandler (req, res) {
+  res.status(404).send()
+})
+app.use(function fiveHundredHandler (err, req, res, next) {
+  console.error(err)
+  res.status(500).send()
+})
+
+// Start server
+app.listen(process.env.SERVER_PORT, function (err) {
+  if (err) {
+    return console.error(err)
+  }
+  console.log(`Started at http://localhost:${process.env.SERVER_PORT}`)
+})
