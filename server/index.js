2f397576f8e462c9aa17c48f06fdd778ab122bd9
iole <eloi@gonzalezurbano.com>
2020-05-19 16:53:39 -0700

    Basic socket setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 'use strict'
 const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
 require('dotenv').config() // Setup dotenv
 
 // Create the express app
-const app = express()
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Routes and middleware
 // app.use(/* ... */)
@@ -18,9 +22,13 @@ app.use(function fiveHundredHandler (err, req, res, next) {
   res.status(500).send()
 })
 
+io.on('connect', socket => {
+  
+})
+
 // Start server
 const port = process.env.SERVER_PORT || 3000 // Default to 3000
-app.listen(process.env.SERVER_PORT, function (err) {
+server.listen(process.env.SERVER_PORT, function (err) {
   if (err) {
     return console.error(err)
   }
