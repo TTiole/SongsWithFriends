c29944d7573310a4fea29788968e63c36498270b
iole <eloi@gonzalezurbano.com>
2020-05-20 09:44:00 -0700

    Implemented objects and global variables

--- server/rooms.js
+++ server/rooms.js
@@ -18,3 +18,14 @@ class Room {
     this.members = this.members.filter(member => member.getId() !== user.getId());
   }
 }
+
+const addRoom = (id, members, host) => rooms.push(new Room(id, members, host))
+const closeRoom = id => {
+  rooms = rooms.filter(room => room.getId() !== id)
+} 
+const getRoom = id => rooms.find(room => room.getId() === id)
+const getRoomByMember = uid => rooms.find(room => room.getMembers().find(user => user.getId() === uid) !== undefined)
+
+module.exports = {
+  addRoom, closeRoom, getRoom, getRoomByMember
+}
--- server/users.js
+++ server/users.js
@@ -1,13 +1,37 @@
-module.exports = class User {
-  constructor(id, name, token, token_type) {
+let users = [];
+class User {
+  constructor(id) {
     this.id = id;
-    this.name = name;
-    this.token = token;
-    this.token_type = token_type;
+    this.name = "";
+    this.token = "";
+    this.token_type = "";
+    this.playback_device = "";
   }
 
   getId = () => this.id;
   getName = () => this.name;
   getToken = () => this.token;
   getTokenType = () => this.token_type;
+  getPlaybackDevice = () => this.playback_device;
+
+  setName = (name) => { this.name = name; }
+
+  auth = (token, token_type) => {
+    this.token = token;
+    this.token_type = token_type;
+    return this; // return the user object for chaining
+  }
+
+  clientInfo = () => ({name: this.name, id: this.id, playbackDevice: this.playback_device})
 };
+
+const getUser = id => users.find(user => user.getId() === id);
+const authUser = (id, token, token_type) => getUser(id).auth(token, token_type);
+const addUser = id => users.push(new User(id))
+const removeUser = id => {
+  users = users.filter(user => user.id !== id);
+}
+
+module.exports = {
+  getUser, authUser, addUser, removeUser
+}
