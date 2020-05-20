a22f97fc503eaa4b551a5a1d997130ec681f0b8f
David Guo <dvguo@ucdavis.edu>
2020-05-19 19:08:37 -0700

    Got Access Token

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-'use strict'
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
-require('dotenv').config() // Setup dotenv
+"use strict";
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const fetch = require("node-fetch");
+
+require("dotenv").config(); // Setup dotenv
 
 // Create the express app
 const app = express();
@@ -14,23 +16,72 @@ const io = socketio(server);
 // app.get(/* ... */)
 
 // Error handlers
-app.use(function fourOhFourHandler (req, res) {
-  res.status(404).send()
-})
-app.use(function fiveHundredHandler (err, req, res, next) {
-  console.error(err)
-  res.status(500).send()
-})
+// app.use(function fourOhFourHandler(req, res) {
+//   res.status(404).send();
+// });
+// app.use(function fiveHundredHandler(err, req, res, next) {
+//   console.error(err);
+//   res.status(500).send();
+// });
 
-io.on('connect', socket => {
-  
-})
+app.get("/", function (res) {
+  // res.end("Server is up");
+  console.log("Server is up");
+});
+
+// Request authroization from user to access data
+// Current scopes: user-modify-playback-state, playlist-modify-public, user-library-read
+app.get("/login", function (req, res) {
+  var scopes =
+    "user-modify-playback-state playlist-modify-public user-library-read";
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize" +
+      "?response_type=code" +
+      "&client_id=" +
+      process.env.SPOTIFY_CLIENT_ID +
+      (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
+      "&redirect_uri=" +
+      encodeURIComponent(process.env.REDIRECT_URI)
+  );
+});
+
+app.get("/authSuccess", function (req, res) {
+  reqAccessToken(req.query.code, res);
+});
+
+function reqAccessToken(authCode, res) {
+  fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          process.env.SPOTIFY_CLIENT_ID +
+            ":" +
+            process.env.SPOTIFY_CLIENT_SECRET
+        ).toString("base64"),
+    },
+    body: `grant_type=authorization_code&code=${authCode}&redirect_uri=${encodeURIComponent(
+      process.env.REDIRECT_URI
+    )}`,
+  })
+    .then((response) => response.text())
+    .then((data) => res.end(data))
+    .catch((err) => {
+      console.log(err);
+      res.end("end err");
+    });
+}
+
+io.on("connect", (socket) => {});
 
 // Start server
-const port = process.env.SERVER_PORT || 8000 // Default to 8000
+const port = process.env.SERVER_PORT || 8000; // Default to 8000
 server.listen(process.env.SERVER_PORT, function (err) {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
-  console.log(`Started at http://localhost:${process.env.SERVER_PORT}`)
-})
+  console.log(`Started at http://localhost:${process.env.SERVER_PORT}`);
+});
