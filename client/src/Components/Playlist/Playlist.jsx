80f8569546b1a15ea6a73cec65138ff42aa81ba1
David Guo <dvguo@ucdavis.edu>
2020-05-29 15:07:14 -0700

    Added edit button that shows and hides delete button

--- client/src/Components/Playlist/Playlist.jsx
+++ client/src/Components/Playlist/Playlist.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TrackCell from "../TrackCell/TrackCell.jsx";
 import "./Playlist.css";
 
@@ -9,18 +9,31 @@ const ms2time = (ms) => {
 };
 
 const Playlist = (props) => {
+  const [showDelete, setShowDelete] = useState(false);
+  const handleEdit = () => {
+    if (showDelete === false)
+      setShowDelete(true);
+    else
+      setShowDelete(false);
+  }
   return (
     <div className="playlist-container">
-      <Typography margin="5px" fontSize={25} bold color="#eee">
-        {props.playlistName}
-      </Typography>
+      <div className="top-container">
+        <Typography margin="5px" fontSize={25} bold color="#eee">
+          {props.playlistName}
+        </Typography>
+        <button id="editBtn" onClick={handleEdit}>Edit Playlist</button>
+      </div>
       <div className="tracks">
         {props.tracks.map((track) => (
-          <TrackCell
-            key={track.id}
-            duration={ms2time(track.duration_ms)}
-            track={track}
-          />
+          <div className="track-wrapper">
+            {showDelete ? <button id="removeBtn">-</button> : null}
+            <TrackCell
+              key={track.id}
+              duration={ms2time(track.duration_ms)}
+              track={track}
+            />
+          </div>
         ))}
       </div>
     </div>
